5f968ce74f9cd5bfe9410309e5689bdbeb7af566
soo-park <sparkstacks@gmail.com>
2017-05-15 22:11:13 -0700

    StringifyJSON test passed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,61 +9,71 @@ var stringifyJSON = function(obj) {
 
   // base case for undefined
   if (obj === undefined) {
-    result += 'undefined';   
+    return undefined;   
   }
 
   // base case for string
   if (typeof obj === 'string') {
-    result += '"' + obj + '"';
+    return '"' + obj + '"';
   }  
 
   // base case for numbers
   if (typeof obj === 'number') {
-    result += obj.toString();
+    return obj.toString();
   }
-  
+
   // base case for true 
   if (obj === true) {
-    result += 'true';
+    return 'true';
   }   
 
   // base case for false
   if (obj === false) {
-    result += 'false';
+    return 'false';
   }
 
   if (typeof obj === 'object') {
-    
     // checking array case
     if (Array.isArray(obj)) {
-      console.log('yay', obj, result);
-      for (let i = 0; i < obj.length; i++) {
-        result += '[' + stringifyJSON(obj[i]) + ', ' + ']';
+      result += '[';
+      if (obj.length >0){
+        for (let i = 0; i < obj.length; i++) {
+          result += stringifyJSON(obj[i]) + ',';
+        }
       }
+      result += ']';
     } else if (obj === null) {
       result += 'null';
     } else {
       // object literal goes here
-      for (let key in obj) {
-        result += '{';
-        result += ' "' + key + '":' + stringifyJSON(obj[key]) + ', ';
-        result += '}';
+      // debugger;
+      result += '{';
+      if (Object.keys(obj).length > 0) {
+        for (var key in obj) {
+          if (key === undefined || obj[key] === undefined || typeof key === "function" || typeof obj[key] === "function") {
+            let a= 1;
+          } else {
+            result += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+          }
+        }
       }
+      
+      result += '}';
     }
   }
 
+  let prev = result[0];
+  var result1 = '';
+  for (let j = 1; j < result.length; j++) {
+    if (prev === ',' && (result[j] === ']' || result[j] === '}')) {
+      prev = result[j];
+    } else {
+      result1 += prev;
+      prev = result[j];
+    }
+  }
+  result1+= result[result.length-1];
+  result = result1; 
+
   return result;
-
-  // let prev = result[0];
-  // var result1 = '';
-  // for (let j = 1; j < result.length; j++) {
-  //   if (prev === ',' && (result[i] === ']' || result[i] === '}')) {
-  //     prev = result[i];
-  //   } else {
-  //     result1 += prev;
-  //     prev = result[i];
-  //   }
-  // }
-
-  // return result1;
 };
